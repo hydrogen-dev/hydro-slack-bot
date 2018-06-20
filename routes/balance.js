7a806ba9b36c3f9075c59c7f6fa84fc8a8b4b73d
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-06-20 16:07:58 -0400

    add name support for important addresses

--- routes/balance.js
+++ routes/balance.js
@@ -5,8 +5,13 @@ let balanceToEther = (weiBalance) => {
   return parseInt(weiBalance) / 1e18
 }
 
+let addressNames = {
+  '0x0fccb4868b7f13ede288aff9298fce67541e3d38': 'Mainnet Deployment Address'
+}
+
 router.get('/:address', function (req, res, next) {
   let address = req.params.address
+  let addressName = address.toLowerCase() in addressNames ? addressNames[address.toLowerCase()] : undefined
 
   req.app.get('getBalanceEtherscan')(address, req.app.get('ETHERSCAN_API_KEY'))
     .then(async balance => {
@@ -14,11 +19,11 @@ router.get('/:address', function (req, res, next) {
       if (etherBalance < 0.1) {
         await req.app.get('sendWebhook')(
           req.app.get('webhooks').noah,
-          '@channel: ⚠️Low Balance Warning⚠️',
+          '@channel ⚠️Low Balance Warning⚠️',
           [{
             'fallback': `The current balance of ${address} is *${etherBalance.toFixed(4)}* ETH`,
-            'color': '#F1CC29',
-            'title': address,
+            'color': 'warning',
+            'title': addressName ? `${addressName}\n${address}` : address,
             'title_link': `https://etherscan.io/address/${address}`,
             'text': `Current balance: *${etherBalance.toFixed(4)}* ETH`,
             'ts': Math.floor(new Date() / 1000)
