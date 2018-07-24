26efe9a0a2fabb8ce2749452b59000fb323d8a60
Noah Zinsmeister <noah@hydrogenplatform.com>
2018-07-23 22:49:53 -0400

    regenerate slack token and move to .env

--- routes/slashCommands.js
+++ routes/slashCommands.js
@@ -11,7 +11,7 @@ const validCommands = {
 }
 
 router.post('/', function (req, res, next) {
-  if (req.body.token !== 'dqciCjH7GcQqFOpNYtSdKAwm') throw Error('Unauthorized')
+  if (req.body.token !== req.app.get('SLACK_TOKEN')) throw Error('Unauthorized')
   if (!Object.keys(validCommands).includes(req.body.command)) throw Error(`Unsupported command: ${req.body.command}`)
 
   validCommands[req.body.command](res)
